6acc8002e5e446e82f57a91dbf34e7694e2305c7
yellowBunny <yellowBunny@gmail.com>
2018-08-22 13:13:06 +0000

    added first simple  unittest

--- csv_mod.py
+++ csv_mod.py
@@ -12,31 +12,31 @@ class Csv_mod():
         
         self.path = ''
         self.data = ''        
-        self.headers = ['field1', 'field2']
-        #self.main()       
+        self.headers = ['field1', 'field2']              
     
-    def write(self, path, data):
-        '''Write file to .csv file '''
+    def write(self, path, data, headers):
+        '''Write file to .csv file '''        
         try:
             with open(path, 'r') as f:
                 c = 1
         except:
             c = 0
             
-        with open(self.path, 'a') as file:                       
-            csv_write = csv.DictWriter(file, fieldnames=self.headers)
+        with open(path, 'a') as file:                       
+            csv_write = csv.DictWriter(file, fieldnames=headers)
             if not c:
                 csv_write.writeheader()
-            d = {key: val for key, val in zip(self.headers, self.data)}
+            d = {key: val for key, val in zip(self.headers, data)}
             csv_write.writerow(d)
-            print('data was writer')
+            print('data was writer')            
+        return path.split('/')[-1]
             
     def main(self):        
         if len(self.headers) < 2:
             print('append header to filed_names')
         else:
             if self.path and self.data:
-                print(self.write(self.path, self.data))
+                print(self.write(self.path, self.data, self.headers))
             else:
                 print('set path and data')
                         
--- unitesty.py
+++ unitesty.py
@@ -1,7 +1,31 @@
-from unittest import TestCase
-import csv_mod
+import unittest 
+from csv_mod import Csv_mod
+import os
+import csv
 
-class Sensor_tests(TestCase):
+class Sensor_tests(unittest.TestCase):
     
-    def test_simple_csv_test(self):
-        pass
+    def test_simple_csv_test_write_func(self):
+        sample_path = '/home/pi/Desktop/test.csv'
+        data = ['third', 'fourth']
+        headers = Csv_mod().headers
+        func = Csv_mod().write(sample_path, data, headers)        
+        dict_with_saved_data = {key : val for key, val in zip(headers, data)}
+        
+        file_localization = '/'.join(s for s in sample_path.split('/')[:-1])
+        self.assertIn(func, os.listdir(file_localization))
+        
+        with open(sample_path, 'r') as file:
+            csv_read = csv.DictReader(file)
+            for line in csv_read:
+                print(dict_with_saved_data)
+                print(line)
+                self.assertEqual(line, dict_with_saved_data, 'should be {} is {}'.format(dict_with_saved_data, line))
+                if line == dict_with_saved_data:
+                    print('ok')
+                else:
+                    print('wrong')
+                    
+unittest.main()
+    
+
