8c454e2daec7963e3a5d9b2287f5e7b29d95f5e4
yellowBunny <yellow.bunny51@gmail.com>
2019-02-06 10:24:07 +0100

    added optionsmenus for set temperature to each heaters

--- app.py
+++ app.py
@@ -17,13 +17,23 @@ class MyOptionMenu(tk.OptionMenu):
         if flag:
             content = (num for num in range(n) if not num % 5)
         else:
-            content = range(n)
+            content = self.is_tuple(n)
         tk.OptionMenu.__init__(self, window, self.var, *content)
         self.config(font=font)
         self['menu'].config(font=font)
         
     def get_data(self):
         return self.var.get()
+
+    def is_tuple(self, n):
+        if type(n) is tuple:
+            start, stop = n
+            # print('tuple')
+            return range(start, stop)
+        else:
+            # print('not tuple')
+            return range(n)
+
         
 class MyButtons(tk.Button):
     def __init__(self, window, title, command):
@@ -34,7 +44,7 @@ class MyApp:
 \t\tWindow steering for turn 'ON' and turn 'OFF' any kind of devices'''
     root = tk.Tk()
     root.geometry('600x400')
-    root.title('home ster')
+    root.title('Home Steering')
     ###FRAMES###
     frame_time = tk.Frame(master=root)
     frame_time.grid(row=0,column=2, stick='N')
@@ -42,18 +52,17 @@ class MyApp:
     frame_other.grid(row=0)
     PIN_SOCKETS = 21
     ###TEMP PINS###
-    PIN_OUTSIDE = 12
-    PIN_SALON = 26
-    PIN_POKOJ1 = 20
-    PIN_POKOJ2 = 16
-    PIN_KUCHNIA = 12
+    PIN_OUTSIDE = 12 #0
+    PIN_SALON = 26   #1
+    PIN_POKOJ1 = 20  #2
+    PIN_POKOJ2 = 16  #3
+    PIN_KUCHNIA = 12 #4
 
-
+    ###KEEP CURENT TEMPERATURE IN ROOMS where look abowe###
     temp_container = [0,0,0,0,0,0]
     
     def __init__(self):
         self.main()
-        
         
     def wigets(self):
         fonts = [('Arial', 10),('Arial', 8)]
@@ -63,9 +72,9 @@ class MyApp:
         lb2 = tk.Label(master=self.frame_other, text='Ustaw minute')
         lb2.grid(row=0,column=1)
         self.lb_show_time = tk.Label(master=self.frame_time, text='time', font=('Arial',18), fg='blue')
-        self.lb_show_time.grid(row=0, column=2, stick='E')
+        self.lb_show_time.grid(row=0, column=0, stick='W')
         self.lb_show_date = tk.Label(master=self.frame_time, text='date', font=('Arial',18), fg='green')
-        self.lb_show_date.grid(row=1, column=2, stick='E')
+        self.lb_show_date.grid(row=1, column=0, stick='W')
         self.lb_show_status = tk.Label(master=self.frame_other, text = 'status')
         self.lb_show_status.grid(row=5, column=0)
         ###TEMP LABELS###
@@ -94,10 +103,15 @@ class MyApp:
         self.view_temp5 = tk.Label(master=self.frame_other, text='temp5')
         self.view_temp5.grid(row=5, column=4)
         self.view_temp6 = tk.Label(master=self.frame_other, text='None')
-        self.view_temp6.grid(row=6, column=4)   
+        self.view_temp6.grid(row=6, column=4)
+        ##view seted temperature in each room##
+        self.view_seted_temp = tk.Label(master=self.frame_time,
+                                        text='Ustawienie\nSalon: {}\nPokój1: {}\nLazienka: {}'.format('n/a','n/a','n/a'))
+        self.view_seted_temp.grid(row=3, column=0, stick='W')
         
         
-        ####OptionMenu####
+        ####OPTIONMENU####
+        ##choose time to switch on/off sockets##
         self.opt_men1 = MyOptionMenu(window=self.frame_other, title='godzina', n=24, font=fonts[0])
         self.opt_men1.grid(row=1,column=0)
         self.opt_men2 = MyOptionMenu(window=self.frame_other, title='minuta', n=60, font=fonts[1], flag=1)
@@ -106,6 +120,14 @@ class MyApp:
         self.opt_men3.grid(row=2,column=0)
         self.opt_men4 = MyOptionMenu(window=self.frame_other, title='minuta', n=60, font=fonts[1], flag=1)
         self.opt_men4.grid(row=2,column=1)
+        ##choose temperature when heaters valve turn on or turn off
+        temp_range = (19,24)
+        self.opt_heater1 = MyOptionMenu(window=self.frame_other, title='salon', n=temp_range, font=fonts[1])
+        self.opt_heater1.grid(row=2, column=5)
+        self.opt_heater2 = MyOptionMenu(window=self.frame_other, title='Pokój1', n=temp_range, font=fonts[1])
+        self.opt_heater2.grid(row=3,column=5)
+        self.opt_heater3 = MyOptionMenu(window=self.frame_other, title='Kuchnia', n=temp_range, font=fonts[1])
+        self.opt_heater3.grid(row=5, column=5)
         ###BUTTONS###
 ##        b_set_time = tk.Button(self.frame_other, text='Ustaw czas',
 ##                               command=lambda: self.set_time(self.opt_men1, self.opt_men2, self.opt_men3, self.opt_men4))
@@ -214,7 +236,7 @@ class MyApp:
         self.update_time()
     
         
-        
-start = MyApp()
-start.root.mainloop()
+if __name__ == '__main__':
+    start = MyApp()
+    start.root.mainloop()
     
